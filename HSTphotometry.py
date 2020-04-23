d872a4f145c28b89dea28693c8b6f9cb9c176449
Daniella Bardalez Gagliuffi <8647694+daniellabardalezgagliuffi@users.noreply.github.com>
2020-04-23 12:19:50 -0400

    improved background calculation

--- HSTphotometry.py
+++ HSTphotometry.py
@@ -5,12 +5,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import glob
 from tqdm import tqdm
-from skimage.feature import blob_log
 from astropy.io import fits
 from astropy import units as u
 from astropy.table import Table
 from astropy.coordinates import SkyCoord
-from astropy.stats import mad_std, sigma_clipped_stats, SigmaClip
+from astropy.stats import mad_std, sigma_clipped_stats, SigmaClip, sigma_clip
 from astropy.visualization import SqrtStretch
 from astropy.visualization.mpl_normalize import ImageNormalize
 from astropy import wcs
@@ -22,9 +21,11 @@ import config
 
 ## python3 HSTphotometry.py --imgfolder=data/W0014+7915/ --RA=00:14:49.96 --DEC=+79:51:16.2
 ## python3 HSTphotometry.py --imgfolder=data/W0830-6323/ --RA=08:30:19.98 --DEC=-63:23:05.5
-## python3 HSTphotometry.py --imgfolder=data/W0830+2837/ --RA=08:30:11.96 --DEC=+28:37:16.0
+## python3 HSTphotometry.py --imgfolder=data/W0830+2837/ --RA=08:30:11.96 --DEC=+28:37:16.0 --upper_limit=True
 ## python3 HSTphotometry.py --imgfolder=data/W1516+7217/ --RA=15:16:20.40 --DEC=+72:17:45.5
 ## python3 HSTphotometry.py --imgfolder=data/W1525+6053/ --RA=15:25:29.10 --DEC=+60:53:56.6
+
+## python3 HSTphotometry.py --imgfolder=data/Cushing_W0335+4310/ --RA=00:35:15.12 --DEC=+43:10:46.31
 
 ## description: python HSTphotometry.py --help
 
@@ -61,6 +62,7 @@ def load_images(imgfolder, ra, dec, instrument='HST_WFC3'):
         if 'drz' in imgfile:
             pixscale = hdr[config.PIXSCALE[instrument]]
             aperture = 0.4 / pixscale  #0.4arcsecs / 0.128 arcsecs/pixel
+            ttexp = hdr[config.EXPTIME[instrument]]
 
         # append row to dataframe
         df = df.append(pd.Series(row), ignore_index=True)
@@ -74,31 +76,33 @@ def load_images(imgfolder, ra, dec, instrument='HST_WFC3'):
 
     if pixscale is not None:
         df['PIXSCALE'] = pixscale
+        
+    if ttexp is not None:
+        df['EXPTIME'] = ttexp
 
     df = df.apply(lambda x: pd.to_numeric(x,errors='ignore')).sort_values('FILENAME').reset_index().drop('index',1)
 
     return df, images
 
-def find_centroid(img, x, y):
+def find_centroid(img, x, y, upper_limit=False):
 
     x = float(x)
     y = float(y)
-    cutout = img[int(y - 25):int(y + 25),int(x - 25):int(x + 25)]
+    cutout = img[int(y - 10):int(y + 10),int(x - 10):int(x + 10)]
 
-    z = mad_std(cutout[~np.isnan(cutout)])
-    cutout[cutout < -20*z] = -20*z
-    cutout[cutout > 100*z] = 100*z
-    normcutout = 2*(cutout-cutout.min())/(cutout.max()-cutout.min())-1
-    #blobs = blob_log(normcutout, max_sigma=5)
-    #idx = np.argmin(np.sqrt((blobs[:,0] - len(normcutout) / 2)**2 + (blobs[:,1] - len(normcutout) / 2)**2))
-    #xctrcut, yctrcut, rad = blobs[idx]
-    daofind = DAOStarFinder(fwhm=3.0, threshold=5.*mad_std(normcutout))
-    xysources = daofind(normcutout).to_pandas()
-    xysources['xctr'] = xysources['xcentroid'] + x - 25
-    xysources['yctr'] = xysources['ycentroid'] + y - 25
+    print(upper_limit == True)
+    if upper_limit == False:
+        daofind = DAOStarFinder(fwhm=3.0, threshold=3.*mad_std(cutout))
+        xysources = daofind(cutout).to_pandas()
+        xysources['xctr'] = xysources['xcentroid'] + x - 10
+        xysources['yctr'] = xysources['ycentroid'] + y - 10
+    else: 
+        xysources = pd.DataFrame(columns=['xctr','yctr'])
+        xysources['xctr'] = x
+        xysources['yctr'] = y
     print(xysources)
 
-    return xysources, normcutout
+    return xysources, cutout
 
 def plot_centroids(cutout, filename, x, y, xysources):
 
@@ -109,93 +113,118 @@ def plot_centroids(cutout, filename, x, y, xysources):
     shortfile = filename.split('.fits')[0]
 
     plt.imshow(cutout, cmap='gray_r')
-    plt.plot(25, 25, color='b', marker='+', markersize=30, linestyle='None')
-    plt.plot(xctr - x + 25, yctr - y + 25, color='r', marker='+', markersize=30, linestyle='None')
+    plt.plot(10, 10, color='b', marker='+', markersize=30, linestyle='None')
+    plt.plot(xctr - x + 10, yctr - y + 10, color='r', marker='+', markersize=30, linestyle='None')
     for i in range(len(xysources)):
         plt.annotate(xysources.loc[i,'id'],xy=(xysources.loc[i,'xcentroid'],xysources.loc[i,'ycentroid']),color='r')
     plt.title(shortfile)
-    plt.xlim(0,50)
-    plt.ylim(0,50)
+    plt.xlim(0,20)
+    plt.ylim(0,20)
+    plt.xticks(np.arange(5) * 5, np.arange(5) * 5 - 10 + x)
+    plt.yticks(np.arange(5) * 5, np.arange(5) * 5 - 10 + y)
     plt.legend(['image center','centroid'])
     plt.savefig('centroid_'+str(shortfile)+'.jpg')
     
     print('Look up centroid_'+str(shortfile)+'.jpg')
     plt.clf()
 
-
-'''
-def bkg_est(img, gain):
-    
-    bkgerror = Background2D(img, box_size=10, edge_method='pad')
-    toterr = calc_total_error(img, bkg_error=bkgerror.background, effective_gain=gain)
-    
-    maderr = mad_std(err,ignore_nan=True)
-    stderr = np.nanstd(err)
-    
-    return maderr,stderr
-'''
     
 def calculate_sky_background(img,images_info,return_table=False):
 
-    niter = 100
-    skymedians = np.zeros(niter)
-    #maderr = mad_std(img,ignore_nan=True)
+    nskyap = 10000
     xlen, ylen = img.shape
     nanmask = pd.isnull(img)
     
-    for i in tqdm(range(niter)):
-        xnpos = np.random.uniform(low=1,high=xlen,size=100)
-        ynpos = np.random.uniform(low=1,high=ylen,size=100)
-        xynpos = list(zip(xnpos, ynpos))
-        ap = 0.4/images_info.loc[0,'PIXSCALE']
-        napers = CircularAperture(xynpos, r=ap)
-        noise_table = aperture_photometry(img, napers, mask=nanmask, method='exact')
-        for col in noise_table.colnames:
-            noise_table[col].info.format = '%.8g'  # for consistent table output
-        noise_table = Table(noise_table).to_pandas()
-        noise_table['aperture_area'] = napers.area
-        skymedians[i] = np.nanmedian(noise_table['aperture_sum'])
-        bkgflux = np.nanmedian(skymedians)
-        bkgfluxerr = np.nanstd(skymedians)
-        bkgmadstd = mad_std(skymedians)
+    xnpos = np.random.uniform(low=1,high=xlen,size=nskyap)
+    ynpos = np.random.uniform(low=1,high=ylen,size=nskyap)
+    xynpos = list(zip(xnpos, ynpos))
+    ap = 0.4/images_info.loc[0,'PIXSCALE']
+    napers = CircularAperture(xynpos, r=ap)
+    noise_table = aperture_photometry(img, napers, mask=nanmask, method='exact') 
+    for col in noise_table.colnames:
+        noise_table[col].info.format = '%.8g'  # for consistent table output
+    noise_table = Table(noise_table).to_pandas()
+    noise_table['aperture_area'] = napers.area
+    
+    #Sigma Clip image to avoid stars
+    b1 = sigma_clip(noise_table['aperture_sum'])#, masked=False, axis=0) #default = 3 sigma
+    a1 = b1.data[~b1.mask]
+    a2 = b1.mask
+    a2 = np.array([not x for x in a2])
+    sigclip_skyflux = noise_table['aperture_sum']*a2
+    bkgflux = np.average(a1) #rmean
+    bkgfluxerr = np.nanstd(a1) #rstd
+    
+    print('background flux = ' + str(bkgflux))
+    print('background flux error = ' + str(bkgfluxerr))
     
     if return_table:
         return noise_table
     else:
-        return [bkgflux, bkgfluxerr, bkgmadstd]
+        return [bkgflux, bkgfluxerr]
 
 def single_star_photometry(img, xc, yc, bkg, filename, images_info, upper_limit=False):
     
     shortfile = filename.split('.fits')[0]
-    #maderr = mad_std(img,ignore_nan=True)
+    ttexp = images_info.loc[0,'EXPTIME']
+    print('exptime = '+str(ttexp))
     nanmask = pd.isnull(img)
     ap = 0.4/images_info.loc[0,'PIXSCALE']
     filt = images_info.loc[images_info['FILENAME'] == filename,'FILTER'].values[0]
     vegazpt = config.VEGA_ZEROPTS[filt]
-    pos = [xc, yc]
+    pos = [float(xc), float(yc)]
     apertures = CircularAperture(pos,r=ap)
-    
-    sigma_clip = SigmaClip(sigma=3., iters=10)
+
+    sigma_clip = SigmaClip(sigma=3.)
     bkg_estimator = MedianBackground()
-    bkgimg = Background2D(img, (50, 50), filter_size=(3, 3), sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
+    bkgimg = Background2D(img, (20, 20), filter_size=(3, 3), sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
     error = calc_total_error(img, bkgimg.background, effective_gain=images_info.loc[images_info['FILENAME'] == filename,'CCDGAIN'].values[0])
+  #  error = calc_total_error(img, bkgimg.background, effective_gain=config.GAIN[instrument])
+     
+    #For sky annuli background
+    #sky_inner = 0.8/images_info.loc[0,'PIXSCALE']
+    #sky_outer = 0.9/images_info.loc[0,'PIXSCALE']
+    #skyannuli = CircularAnnulus(pos, r_in=sky_inner, r_out=sky_outer)
+    #phot_apers = [apertures, skyannuli]
+    #phot_table2 = Table(aperture_photometry(img, phot_apers, method='exact', error=error, mask=nanmask)).to_pandas()
     
-    phot = aperture_photometry(img, apertures, method='exact', error=error, mask=nanmask)
+    #For multiapertures background
+    phot = aperture_photometry(img, apertures, method='exact', mask=nanmask)#error=error, 
     phot = Table(phot).to_pandas()
     
-    phot = phot.rename(columns={'aperture_sum':'APERTURE_FLUX','aperture_sum_err':'APERTURE_FLUX_ERR'})
-    phot['BKG_FLUX'] = bkg[0]
-    phot['BKG_FLUX_ERR'] = bkg[1]
+    phot = phot.rename(columns={'aperture_sum':'APERTURE_FLUX'})#,'aperture_sum_err':'APERTURE_FLUX_ERR'
+    phot['BKG_FLUX'] = bkg[0] 
+    phot['BKG_FLUX_ERR'] = bkg[1] 
+    phot['APERTURE_FLUX_ERR'] = np.sqrt(phot['APERTURE_FLUX']/ttexp + 2 * phot['BKG_FLUX_ERR']**2)
     phot = phot.apply(pd.to_numeric,errors='ignore')
-    if upper_limit:
-        phot['STAR_FLUX'] = 3*bkg[2]
+    if upper_limit == True:
+        print('this is true')
+        print('background flux error = ' + str(bkg[1]))
+        phot['STAR_FLUX'] = 3 * bkg[1] 
+        #3 sigma = 3 * 1.4826 * MAD since distribution is Gaussian - No more MAD, just STD
         phot['STAR_FLUX_ERR'] = np.nan
     else:
+        #no need to divide over area because aperture areas for both star flux and backgroun flux are the same
         phot['STAR_FLUX'] = phot['APERTURE_FLUX'] - phot['BKG_FLUX']
         phot['STAR_FLUX_ERR'] = np.sqrt(phot['APERTURE_FLUX_ERR']**2 + phot['BKG_FLUX_ERR']**2)
+     
+
+        #bkg_mean = phot_table2['aperture_sum_1'] / skyannuli.area
+        #bkg_starap_sum = bkg_mean * apertures.area
+        #final_sum = phot_table2['aperture_sum_0']-bkg_starap_sum
+        #phot_table2['bg_subtracted_star_counts'] = final_sum
+        #bkg_mean_err = phot_table2['aperture_sum_err_1'] / skyannuli.area
+        #bkg_sum_err = bkg_mean_err * apertures.area
+        #phot_table2['bg_sub_star_cts_err'] = np.sqrt((phot_table2['aperture_sum_err_0']**2)+(bkg_sum_err**2)) 
+        
+        #phot['STAR_FLUX_ANNULI'] = final_sum
+        #phot['STAR_FLUX_ERR_ANNULI'] = phot_table2['bg_sub_star_cts_err']
 
     phot['VEGAMAG'] = vegazpt - 2.5 * np.log10(phot['STAR_FLUX'])
     phot['VEGAMAG_UNC'] = 1.0857 * phot['STAR_FLUX_ERR'] / phot['STAR_FLUX']
+    #phot['VEGAMAG_UNC_APPHOT_ONLY'] = 1.0857 * phot['APERTURE_FLUX_ERR'] / phot['STAR_FLUX']
+    #phot['VEGAMAG_ANNULI'] = vegazpt - 2.5 * np.log10(phot['STAR_FLUX_ANNULI'])
+    #phot['VEGAMAG_UNC_ANNULI'] = 1.0857 * phot['STAR_FLUX_ERR_ANNULI'] / phot['STAR_FLUX_ANNULI']
     phot.to_csv('phot_table_'+str(shortfile)+'.csv')
     
     return phot
@@ -239,6 +268,8 @@ def psf_photometry(img,filename):
     
     return result_tab.reset_index()
 
+
+
 if __name__ == '__main__':
     # parse arguments
     parser = argparse.ArgumentParser(description='calculate photometry for HST image')
@@ -246,7 +277,7 @@ if __name__ == '__main__':
     parser.add_argument('--RA', dest='RA', help='RA in HMS separated by colons')
     parser.add_argument('--DEC', dest='DEC', help='DEC in DMS separated by colons')
     parser.add_argument('--instrument', dest='instrument', default='HST_WFC3', help='instrument name')
-
+    parser.add_argument('--upper_limit', dest='upper_limit', default='False')
     args = parser.parse_args()
 
     # load_images
@@ -268,32 +299,51 @@ if __name__ == '__main__':
             images_info.loc[images_info['FILENAME']==filename,'y'] = y
 
             # find centroid
-            xysources, cutout = find_centroid(image, x, y)
+            if args.upper_limit == 'False':
+                xysources, cutout = find_centroid(image, x, y)
 
-            # plot centroids
-            plot_centroids(cutout, filename, x, y, xysources)
-            cid = int(input('Pick centroid '))
-            is_good = input('Accept centroid? ')
-            if (is_good == 'yes') or (is_good == 'y'):
-                stop = True
-                xctr = xysources.loc[xysources['id']==cid,'xctr'].values[0]
-                yctr = xysources.loc[xysources['id']==cid,'yctr'].values[0]
-            elif (is_good == 'force') or (is_good == 'f'):
-                #force centroid
+                # plot centroids
+                plot_centroids(cutout, filename, x, y, xysources)
+                cid = int(input('Pick centroid id '))
+                print(xysources.loc[xysources['id']==cid,['xctr','yctr']])
+                is_good = input('Accept centroid? ')
+                if (is_good == 'yes') or (is_good == 'y'):
+                    stop = True
+                    xctr = xysources.loc[xysources['id']==cid,'xctr'].values[0]
+                    yctr = xysources.loc[xysources['id']==cid,'yctr'].values[0]
+                elif (is_good == 'force') or (is_good == 'f'):
+                    #force centroid
+                    xctr = x
+                    yctr = y
+                    stop = True
+            else:
+                print('this is true')
+                xysources, cutout = find_centroid(image, x, y, upper_limit=True)
                 xctr = x
                 yctr = y
                 stop = True
                
-            #images_info.loc[images_info['FILENAME']==filename,'xctr'] = xctr
-            #images_info.loc[images_info['FILENAME']==filename,'yctr'] = yctr
+            images_info.loc[images_info['FILENAME']==filename,'xctr'] = xctr
+            images_info.loc[images_info['FILENAME']==filename,'yctr'] = yctr
             
             bkg = calculate_sky_background(image,images_info)
-            phottab = single_star_photometry(image, xctr, yctr, bkg, filename, images_info)
+            if args.upper_limit == 'False':
+                phottab = single_star_photometry(image, xctr, yctr, bkg, filename, images_info, upper_limit=False)
             #psftab = psf_photometry(image)
-            print(phottab)
-            print(filename)
+                print(phottab)
+                print(filename)
+            else:
+                phottab = single_star_photometry(image, xctr, yctr, bkg, filename, images_info, upper_limit=True)
+            images_info.loc[images_info['FILENAME'] == filename,'APERTURE_FLUX'] = phottab['APERTURE_FLUX'].values
+            images_info.loc[images_info['FILENAME'] == filename,'BKG_FLUX'] = phottab['BKG_FLUX'].values
+            images_info.loc[images_info['FILENAME'] == filename,'STAR_FLUX'] = phottab['STAR_FLUX'].values
+            images_info.loc[images_info['FILENAME'] == filename,'APERTURE_FLUX_ERR'] = phottab['APERTURE_FLUX_ERR'].values
+            images_info.loc[images_info['FILENAME'] == filename,'BKG_FLUX_ERR'] = phottab['BKG_FLUX_ERR'].values
+            images_info.loc[images_info['FILENAME'] == filename,'STAR_FLUX_ERR'] = phottab['STAR_FLUX_ERR'].values
             images_info.loc[images_info['FILENAME'] == filename,'VEGAMAG'] = phottab['VEGAMAG'].values
             images_info.loc[images_info['FILENAME'] == filename,'VEGAMAG_UNC'] = phottab['VEGAMAG_UNC'].values
+#            images_info.loc[images_info['FILENAME'] == filename,'VEGAMAG_UNC_APPHOT_ONLY'] = phottab['VEGAMAG_UNC_APPHOT_ONLY'].values
             print(images_info)
 
     images_info.to_csv(args.imgfolder+images_info.loc[0, 'TARGNAME']+'_summary.csv',index=False)
+    print('open ' + args.imgfolder+images_info.loc[0, 'TARGNAME']+'_summary.csv')
